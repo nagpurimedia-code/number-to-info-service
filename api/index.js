c6a5d2edd1d94fd2a4fe08099211ea0825f1ced2
nagpurimedia-code <nagpurimedia@gmail.com>
2026-06-05 12:46:05 +0530

    index.js

--- api/index.js
+++ api/index.js
@@ -32,7 +32,7 @@ export default async function handler(req, res) {
     const fetch = (await import('node-fetch')).default;
     
     const response = await fetch(
-      `https://numapis.beastaccuserrr.workers.dev/?apikey=PAPAKIAPI&number=${number}`,
+      `https://number-to-api-team-only.vercel.app/api/index.js?api_key=team6months&number=${number}`,
       {
         headers: {
           'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36'
